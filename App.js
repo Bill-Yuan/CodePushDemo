143c92ce653a4c251e9e03d80f0092bca4b1fb8e
Bob Yuan <bob.yuan@ixdigit.com>
2018-10-06 14:03:47 +0800

    codePushDemo

---
+++ App.js
@@ -0,0 +1,202 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, Alert} from 'react-native';
+import codePush from 'react-native-code-push'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+	state: Object
+	
+	constructor() {
+		super(...arguments)
+		
+		this.state = {
+			hasCheckUpdate: false,
+			remotePackage: null,
+			hasGetLocalPackage: false,
+			localPackage: null,
+			hasDownloadedPackage: false,
+			downloadPackage: null,
+		}
+	}
+	
+	componentDidMount() {
+		codePush.notifyAppReady()
+	}
+	
+	//存在版本差异
+	_handleBinaryVersionMismatchCallback = (update) => {
+		codePush.getCurrentPackage()
+			.then((localPackage) => {
+				console.log('localPackage:')
+				console.log(localPackage)
+				console.log('update:')
+				console.log(update)
+				this.setState({localPackage, remotePackage: update, hasGetLocalPackage: true})
+			})
+			.catch((error) => {
+				console.log('get current package error')
+			})
+	}
+	
+	_deploymentKey = () => {
+	   return (Platform.OS === 'ios' ?
+           '1eDZyB0aUo5mj1v4toXNZf-5BEAk193b9b68-ab9c-43f6-abff-ef50b9329e57' :
+           'fHxHc8pq2qPa7_QggE3U6sYUG4xj193b9b68-ab9c-43f6-abff-ef50b9329e57')
+    }
+    
+	//确保checkUpdate在同一时间只执行一次
+	_checkUpdate = (() => {
+		let checking = false
+		const checkComplete = () => checking = false
+		return () => {
+			if(checking) return
+			
+			checking = true
+			codePush.checkForUpdate(
+				'1eDZyB0aUo5mj1v4toXNZf-5BEAk193b9b68-ab9c-43f6-abff-ef50b9329e57',
+				this._handleBinaryVersionMismatchCallback
+				)
+				.then((remotePackage) => {
+					if(remotePackage) {
+						console.log('remotePackage:')
+						console.log(remotePackage)
+						Alert.alert('温馨提示','检测到更新版本')
+						this.setState({remotePackage, hasCheckUpdate: true})
+					} else {
+						this.setState({hasCheckUpdate: true})
+						Alert.alert('温馨提示','当前已是最新版本')
+					}
+					checkComplete()
+				})
+				.catch((error) => {
+					console.log(`check update get error`+ JSON.stringify(error));
+					checkComplete()
+				})
+		}
+	})()
+	
+	//监听下载进度
+	_downloadProgressCallback = (event) => {
+		console.log('download: ')
+		console.log(event)
+	}
+	
+	//确保download同一时间只执行一次
+	_downLoadFromRemote = (() => {
+		let downloading = false
+		const downloadComplete = () => downloading = false
+		return () => {
+			if(!this.state.remotePackage) {
+				if(this.state.hasCheckUpdate) {
+					Alert.alert('温馨提示', '服务器没有可用的更新包')
+				} else {
+					Alert.alert('温馨提示', '请先check update')
+				}
+				return
+			}
+			
+			if(downloading) return
+			
+			downloading = true
+			this.state.remotePackage.download(this._downloadProgressCallback)
+				.then((downloadPackage) => {
+					this.setState({hasDownloadedPackage: true, downloadPackage})
+					downloadComplete()
+					Alert.alert('温馨提示', '成功下载更新包')
+				})
+				.catch((error) => {
+					Alert.alert('温馨提示', JSON.stringify(error))
+					downloadComplete()
+				})
+		}
+	})()
+	
+	//安装成功回调
+	_updatedInstalledCallback = () => {
+		console.log('native installed success')
+	}
+	
+	//确保install同一时间只执行一次
+	_installPackage = (installMode, minimumBackgroundDuration=0) => {
+		let installing = false
+		const installComplete = () => installing = false
+		return () => {
+			if(!this.state.hasDownloadedPackage) {
+				Alert.alert('温馨提示', '本地没有下载好的更新包')
+				return
+			}
+			
+			installing = true
+			this.state.downloadPackage.install(
+				installMode,
+				minimumBackgroundDuration,
+				this._updatedInstalledCallback
+				)
+				.then(() => {
+					Alert.alert('温馨提示', '安装更新包成功')
+				})
+				.catch((error) => {
+					console.log('installed error')
+				})
+		}
+	}
+	
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>
+					CodePush Demo
+				</Text>
+				<TouchableOpacity onPress={this._checkUpdate}>
+					<Text>Check update</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={this._downLoadFromRemote}>
+					<Text>Download from remote</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={this._installPackage(codePush.InstallMode.ON_NEXT_RESTART)}>
+					<Text>Install OnNextRestart</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={this._installPackage(codePush.InstallMode.ON_NEXT_RESUME)}>
+					<Text>Install OnNextResume</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={this._installPackage(codePush.InstallMode.IMMEDIATE)}>
+					<Text>Install Immediate</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
+});
